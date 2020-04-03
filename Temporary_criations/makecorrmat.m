4fd0460222a80a01f9e028b1b8ee6964e4612ea8
LexC <alexc1451@gmail.com>
2020-04-03 21:42:30 +0200

    200403

--- Temporary_criations/makecorrmat.m
+++ Temporary_criations/makecorrmat.m
@@ -11,11 +11,9 @@ loc{3}='D:\GD_UNICAMP\IC_NeuroFisica\Projetos\Coleta_NIRS_fMRI_2015-2017\Process
 u=1;
 for i=1:size(loc,2)
     %%
-    cd(loc{i})
-    
-    [~,b]=dirff3('Matrix-VAR.mat');
+    [~,b]=dirffin(3,loc{i},'Matrix-VAR.mat');
     %%
-    for j=1:size(b,2)
+    for j=1:size(b,1)
         %%
         c=strsplit(loc{i},'Protocolo_');
         c=strsplit(c{2},'\');
@@ -29,18 +27,72 @@ for i=1:size(loc,2)
         
         CorrMat(u).Type=c;
         %%
-        c=strsplit(b(j).name,'\');
-        c=strsplit(c{2},'_');
+        c=strsplit(b{j},'\');
+        c=strsplit(c{end-2},'_');
         
         CorrMat(u).Subject=str2num(c{3});
         CorrMat(u).Run=str2num(c{4});
         %%
-        load(b(j).name)
+        load(b{j})
         CorrMat(u).map=map;
         u=u+1;
     end
 end
+%% Making mean matrices
+
+CorrMatMean=CorrMat;
+
+u=2;
+for i=2:size(CorrMat,2)
+    CorrMatMean(i).flag=0;
+    
+    if CorrMatMean(i).Subject==CorrMatMean(i-1).Subject
+        CorrMatMean(i-u+1).map(:,:,u)=CorrMatMean(i).map;
+        CorrMatMean(i).flag=1;
+        u=u+1;
+    else
+        u=2;
+    end
+end
+
+u=1;
+for i=1:size(CorrMat,2)
+    
+    if CorrMatMean(u).flag==1
+        CorrMatMean(u)=[];
+    else
+        u=u+1;
+    end
+end
+
+for i=1:size(CorrMatMean ,2)
+    if size(CorrMatMean(i).map,3)>1
+        CorrMatMean(i).map=fishermean( CorrMatMean(i).map,3);
+    end
+end
+
+CorrMatMean = rmfield(CorrMatMean,'Run');
+CorrMatMean = rmfield(CorrMatMean,'flag');
+
+%% Making Group Mean of matrices
+u=1;w=1;
+for i=1:size(CorrMat,2)
+    switch CorrMat(i).Type
+        case 'Patient'
+            pmap(:,:,u)=CorrMat(i).map;
+            u=u+1;
+        case 'Volunteer'
+            vmap(:,:,w)=CorrMat(i).map;
+            w=w+1;
+    end
+end
+
+CorrMatGroupMean(1).Type='Patient';
+CorrMatGroupMean(1).map=fishermean(pmap,3);
+CorrMatGroupMean(2).Type='Volunteer';
+CorrMatGroupMean(2).map=fishermean(vmap,3);
+
 %%
 
-cd('D:\GD_UNICAMP\IC_NeuroFisica\Projetos\Coleta_NIRS_fMRI_2015-2017\Processed_data\fMRI')
-save('CorrMat.mat','CorrMat')
+save('D:\GD_UNICAMP\IC_NeuroFisica\Projetos\Coleta_NIRS_fMRI_2015-2017\Processed_data\fMRI\CorrMat.mat',...
+    'CorrMat','CorrMatMean','CorrMatGroupMean')
--- dirffin.m
+++ dirffin.m
@@ -44,6 +44,7 @@ folders_in_check=size(strsplit(folders{1},'\'),2);
 
 u=1;
 while folders_in_check < pathsize
+    %%
     for i=u:size(folders,1)
         %%
         cd(folders{i})
@@ -54,25 +55,32 @@ while folders_in_check < pathsize
             [ folders1,files1] = dirff(1,pwd,name);
         end
 
+        if isequal(files1,{'None'})==0
+            files=[files;files1];
+        end
+        
         if isequal(folders1,{'None'})==0
             folders=[folders;folders1];
         else
             break
         end
-        if isequal(files1,{'None'})==0
-            files=[files;files1];
-        end
     end
     folders_in_check=size(strsplit(folders{end},'\'),2);
     u=i+1;
+    %%
 	if size(i,1)==0
         break;
     end
 end
 %% Foot
 
+if size(files,1)>1 && isequal(files{1},'None')==1
+    files(1)=[];
+end
+
 folders=sort(folders);
 files=sort(files);
+
 
 % changing the folders back to the original location
 cd(PATH_OUT)
